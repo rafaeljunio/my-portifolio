61ad6929b47e1d7943e831cad42c3a371ed2040c
Rafael Souza <rafael.souza@iexfy.com>
2024-07-05 11:14:28 -0300

    feat: services page

---
+++ app/_components/services.tsx
@@ -0,0 +1,56 @@
+import {
+  CodeBracketSquareIcon,
+  CommandLineIcon,
+  RocketLaunchIcon,
+} from '@heroicons/react/16/solid'
+
+export const Services = () => {
+  return (
+    <div className="bg-[#121212] pt-[4rem] md:pt-[8rem] pb-[5rem]">
+      <p className="heading">
+        My <span className="text-yellow-400">Services</span>
+      </p>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto items-center gap-[3rem] mt-[4rem] text-white">
+        <div className="bg-red-700 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]">
+          <CodeBracketSquareIcon className="size-[6rem] mx-auto text-[#d3fae8]" />
+          <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">
+            Frontend
+          </h1>
+          <p className="text-[15px] text-[#d3d2d2] font-normal">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+            accusantium, perferendis, fugit aspernatur autem deserunt
+            necessitatibus dignissimos quidem possimus libero quibusdam aperiam
+            et nisi sapiente dolorum. Molestiae dolores ratione voluptatem.
+          </p>
+        </div>
+
+        <div className="bg-orange-700 hover:scale-110 transform transition-all duration-300 uppercase font-semibold text-center p-[2rem]">
+          <RocketLaunchIcon className="size-[6rem] mx-auto text-[#d3fae8]" />
+          <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">
+            Backend
+          </h1>
+          <p className="text-[15px] text-[#d3d2d2] font-normal">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+            accusantium, perferendis, fugit aspernatur autem deserunt
+            necessitatibus dignissimos quidem possimus libero quibusdam aperiam
+            et nisi sapiente dolorum. Molestiae dolores ratione voluptatem.
+          </p>
+        </div>
+
+        <div className="bg-blue-700 hover:scale-110 transform transition-all duration-300 hover:rotate-6 uppercase font-semibold text-center p-[2rem]">
+          <CommandLineIcon className="size-[6rem] mx-auto text-[#d3fae8]" />
+          <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">
+            Fullstack
+          </h1>
+          <p className="text-[15px] text-[#d3d2d2] font-normal">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+            accusantium, perferendis, fugit aspernatur autem deserunt
+            necessitatibus dignissimos quidem possimus libero quibusdam aperiam
+            et nisi sapiente dolorum. Molestiae dolores ratione voluptatem.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { About } from './_components/about'
 import { Hero } from './_components/hero'
 import { MobileNav } from './_components/mobile-nav'
 import { Nav } from './_components/nav'
+import { Services } from './_components/services'
 
 export default function Home() {
   const [nav, setNav] = useState(false)
@@ -13,13 +14,15 @@ export default function Home() {
   const closeNav = () => setNav(false)
 
   return (
-    <div className="">
-      <MobileNav nav={nav} closeNav={closeNav} />
-      <Nav openNav={openNav} />
-
-      <Hero />
+    <div className="overflow-x-hidden">
+      <div>
+        <MobileNav nav={nav} closeNav={closeNav} />
+        <Nav openNav={openNav} />
+        <Hero />
+      </div>
       <div className="relative z-[30]">
         <About />
+        <Services />
       </div>
     </div>
   )
